0661d419e65aec3724c849a27fe0e020c55464a7
blitzstern5 <blitzstern5@gmail.com>
2018-12-20 12:28:21 +0300

    Remove useless lines

--- tests/backend/at20180928/develop/scripts/update_full_nodes.sh
+++ tests/backend/at20180928/develop/scripts/update_full_nodes.sh
@@ -5,10 +5,10 @@ python_bin="python3"
 qs_dir="$HOME/quick-start"
 qs_bin="$qs_dir/manage.sh"
 
-#echo "Restoring DB ..."
-#db_dumps="$(cd "$qs_dir" && find . -mindepth 1 -maxdepth 1 -name 'db-dumps-*' | tail -1)"
-#[ -z "$db_dumps" ] && echo "DB dumps not found" && exit 1
-#(cd "$qs_dir" && ./manage.sh frest $db_dumps)
+echo "Restoring DB ..."
+db_dumps="$(cd "$qs_dir" && find . -mindepth 1 -maxdepth 1 -name 'db-dumps-*' | tail -1)"
+[ -z "$db_dumps" ] && echo "DB dumps not found" && exit 1
+(cd "$qs_dir" && ./manage.sh frest $db_dumps)
 
 priv_key="$($qs_bin priv-key 1)"
 
@@ -22,11 +22,5 @@ tcp_addrs=$($qs_bin int-tcp-addrs | sed -E 's/([0-9]+): (.*)$/--node-tcp-addr=\2
 
 pub_keys=$($qs_bin pub-keys | sed -E 's/([0-9]+): (.*)$/--node-pub-key=\2/' | tr -d '\r' | tr '\n' ' ')
 
-#echo "key_ids: $key_ids"
-#echo "api_urls: $api_urls"
-#echo "tcp_addrs: $tcp_addrs"
-#echo "pub_keys: $pub_keys"
-
 str="(cd $proj_dir && PYTHONPATH=$proj_dir $python_bin $this_dir/update_full_nodes.py --call-priv-key=$priv_key --call-api-url=$api_url $key_ids $api_urls $tcp_addrs $pub_keys)"
-echo "$str"
 eval "$str"
