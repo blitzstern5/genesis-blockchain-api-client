83aa6ffccea5ddacc8ad2237d0dc79de560ccf01
blitzstern5 <blitzstern5@gmail.com>
2018-12-20 10:44:41 +0300

    Add update_sys_params to __main__

--- tests/backend/at20180928/develop/scripts/update_sys_params.py
+++ tests/backend/at20180928/develop/scripts/update_sys_params.py
@@ -5,5 +5,4 @@ from genesis_blockchain_api_client.backend.at20180928.develop.scripts.update_sys
 
 if __name__ == '__main__':
     api_url, priv_key, params = get_update_sys_params_args()
-    print("api_url: %s priv_key: %s params: %s" % (api_url, priv_key, params))
-    #update_sys_params(api_url, priv_key, params)
+    update_sys_params(api_url, priv_key, params)
